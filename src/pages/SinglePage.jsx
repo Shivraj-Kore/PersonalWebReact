f50cb1b54e2546640fd30d7d0918db9b44718e37
Shivraj-Kore <shivrajkore100@gmail.com>
2024-02-22 23:48:46 +0530

    About Me and my Skills added

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -4,22 +4,15 @@
   import ph1 from '../assets/ph1.jpeg'
   import { useState , useEffect } from 'react';
   import ParticleBGHomePage from '../components/ParticleBGHomePage';
-  import ParticleBG from '../components/ParticleBG';
   import sun from "../assets/sun.png"
   import moon from "../assets/moon.png"
-  import myphoto from "../assets/myphoto.jpg"
   import { useTypewriter, Cursor } from 'react-simple-typewriter'
   import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 
-
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ');
-  }
-
   export default function SinglePage() {
-    const [changecolor, setChangecolor] = useState(false);
-    const [icon, setIcon] = useState(true);
+    const [changecolor, setChangecolor] = useState(true);
+    const [icon, setIcon] = useState(false);
 
     const handleClick = () => {
       setChangecolor(!changecolor);
@@ -36,7 +29,7 @@
       <>
       <ParticleBGHomePage/>
       <div className='fixed top-0 w-full z-50'>
-      <Disclosure as="nav" className={`${changecolor ? 'bg-[#ffffff] bg-opacity-40' : 'bg-[#000000] bg-opacity-75'}`}>
+      <Disclosure as="nav" className={`${changecolor ? 'bg-[#ffffff] bg-opacity-40' : 'bg-[#000000] bg-opacity-85'}`}>
         {({ open , close }) => (
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -64,10 +57,7 @@
                           <Link  to="home"
                             smooth={true}
                             duration={500}
-                            className={classNames(
-                              'rounded px-3 py-2 font-medium',
-                              'hover:text-[#a78bfa]/100 text-[#8A00FF]/100'
-                            )}>
+                            className={`rounded px-3 py-2 font-medium cursor-pointer hover:text-[#a78bfa]/100 text-[#8A00FF]/100`}>
                             Home
                           </Link>
                         </li>
@@ -75,10 +65,7 @@
                           <Link  to="about"
                             smooth={true}
                             duration={500}
-                            className={classNames(
-                              'rounded px-3 py-2 font-medium',
-                              'hover:text-[#a78bfa]/100 text-[#8A00FF]/100'
-                            )}>
+                            className={`rounded px-3 py-2 font-medium cursor-pointer hover:text-[#a78bfa]/100 text-[#8A00FF]/100`}>
                             About Me
                           </Link>
                         </li>
@@ -86,10 +73,7 @@
                           <Link  to="myskills"
                             smooth={true}
                             duration={500}
-                            className={classNames(
-                              'rounded px-3 py-2 font-medium',
-                              'hover:text-[#a78bfa]/100 text-[#8A00FF]/100'
-                            )}>
+                            className={`rounded px-3 py-2 font-medium cursor-pointer hover:text-[#a78bfa]/100 text-[#8A00FF]/100`}>
                             My Skills 
                           </Link>
                         </li>
@@ -97,10 +81,7 @@
                           <Link  to="projects"
                             smooth={true}
                             duration={500}
-                            className={classNames(
-                              'rounded px-3 py-2 font-medium',
-                              'hover:text-[#a78bfa]/100 text-[#8A00FF]/100'
-                            )}>
+                            className={`rounded px-3 py-2 font-medium cursor-pointer hover:text-[#a78bfa]/100 text-[#8A00FF]/100`}>
                             My Projects 
                           </Link>
                         </li>
@@ -108,10 +89,7 @@
                           <Link  to="contact"
                             smooth={true}
                             duration={500}
-                            className={classNames(
-                              'rounded px-3 py-2 font-medium',
-                              'hover:text-[#a78bfa]/100 text-[#8A00FF]/100'
-                            )}>
+                            className={`rounded px-3 py-2 font-medium cursor-pointer hover:text-[#a78bfa]/100 text-[#8A00FF]/100`}>
                             Contact
                           </Link>
                         </li>
@@ -147,44 +125,34 @@
 
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2">
-                <ul className="space-y-1 px-2 pb-3 pt-2 hover:text-[#a78bfa]/100  text-[#8A00FF]/100 hover:bg-[#a78bfa]/5  bg-[#8A00FF]/5 rounded">
+                <ul className="space-y-1 px-2 pb-3 pt-2 hover:text-[#a78bfa]/100  text-[#8A00FF]/100 rounded">
                   <li>
-                    <Link to="home" smooth={true} duration={500} onClick={() => close()} className={classNames(
-                      'block rounded px-3 py-2 text-base font-medium text-[#ffffff]',
-                      'hover:bg-gray-700 hover:text-white'
-                    )}>
+                    <Link to="home" smooth={true} duration={500} onClick={() => close()} 
+                          className={`block rounded px-3 py-2  font-bold ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
                       Home
                     </Link>
                   </li>
                   <li>
-                    <Link to="about" smooth={true} duration={500} onClick={() => close()} className={classNames(
-                      'block rounded px-3 py-2 text-base font-medium text-[#ffffff]',
-                      'hover:bg-gray-700 hover:text-white'
-                    )}>
+                    <Link to="about" smooth={true} duration={500} onClick={() => close()} 
+                          className={`block rounded px-3 py-2  font-bold ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
                       About Me
                     </Link>
                   </li>
                   <li>
-                    <Link to="myskills" smooth={true} duration={500} onClick={() => close()} className={classNames(
-                      'block rounded px-3 py-2 text-base font-medium text-[#ffffff]',
-                      'hover:bg-gray-700 hover:text-white'
-                    )}>
+                    <Link to="myskills" smooth={true} duration={500} onClick={() => close()} 
+                          className={`block rounded px-3 py-2   font-bold ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
                       My Skills
                     </Link>
                   </li>
                   <li>
-                    <Link to="projects" smooth={true} duration={500} onClick={() => close()} className={classNames(
-                      'block rounded px-3 py-2 text-base font-medium text-[#ffffff]',
-                      'hover:bg-gray-700 hover:text-white'
-                    )}>
+                    <Link to="projects" smooth={true} duration={500} onClick={() => close()} 
+                          className={`block rounded px-3 py-2   font-bold ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
                       My Projects
                     </Link>
                   </li>
                   <li>
-                    <Link to="contact" smooth={true} duration={500} onClick={() => close()} className={classNames(
-                      'block rounded px-3 py-2 text-base font-medium text-[#ffffff]',
-                      'hover:bg-gray-700 hover:text-white'
-                    )}>
+                    <Link to="contact" smooth={true} duration={500} onClick={() => close()} 
+                          className={`block rounded px-3 py-2   font-bold ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
                       Contact
                     </Link>
                   </li>
@@ -195,11 +163,11 @@
         )}
       </Disclosure>
       </div>
-      <div id="home" className={`${changecolor ? 'bg-[#a4a4a4] bg-opacity-50' : 'bg-[#000000] bg-opacity-65'}`}>
-        <div className={`mt-[64px] lgmt-[0px] h-screen ${changecolor ? 'bg-[#ffffff] bg-opacity-10 ' : 'bg-[#000000] bg-opacity-10'} text-[#ffffff] pt-8 gap-5 lg:gap-0 rounded shadow-lg flex flex-col lg:flex-row`}>
+      <div id="home" className={`m-2 rounded ${changecolor ? 'bg-[#a4a4a4] bg-opacity-50' : 'bg-[#000000] bg-opacity-65'}`}>
+        <div className={`mt-[70px] pb-[32px]   ${changecolor ? 'bg-[#ffffff] bg-opacity-10 ' : 'bg-[#000000] bg-opacity-10'} text-[#ffffff] pt-8 gap-5 lg:gap-0 rounded shadow-lg flex flex-col lg:flex-row`}>
             <div className="md:w-1/2 lg:order-1 order-2 flex justify-center items-center ">
               <img
-                className="w-[300px] h-[600px] object-cover"
+                className="w-[300px] h-[400px] object-cover"
                 src={ph1}
                 alt="placeholder"
               />
@@ -221,9 +189,73 @@
             </div>
         </div>
       </div>
-      <div id='about'>
 
+
+
+      <div id='about' className={`m-2 p-4 rounded ${changecolor ? 'bg-[#a4a4a4] bg-opacity-50' : 'bg-[#000000] bg-opacity-65'}`}>
+        <div className='lg:flex-row flex flex-col justify-center items-center'>
+          <div className='basis-1/2 flex justify-center items-center'>
+            <h1 className='font-bold text-4xl text-[#8A00FF]'>About Me :</h1>
+          </div>
+          <div  className='basis-1/2 flex flex-col justify-center items-center'>
+              <p className={`flex justify-center items-center font-bold text-2xl ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
+                Hi, Im a Fullstack Developer and an Ai/Ml enthusiast from Maharashtra , India. 
+              </p>
+            <br />
+              <ul className={` list-disc flex flex-col justify-center items-center text-xl ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
+                  <div className={`pl-6 flex flex-col justify-center items-center `} >
+                    <li className='list-disc p-1'>
+                      I'm a fullstack developer specialised in frontend using React and backend using Django.
+                    </li>
+                    <li className='list-disc p-1'>
+                      I have build machine learning models in Tensorflow and am currently learning PyTorch .
+                    </li>
+                  </div>
+              </ul>
+          </div>
+        </div>
       </div>
-        </>
+
+
+
+      <div id='myskills' className={`m-2 p-4 rounded ${changecolor ? 'bg-[#a4a4a4] bg-opacity-50' : 'bg-[#000000] bg-opacity-65'}`}>
+        <div className='lg:flex-row flex flex-col justify-center items-center'>
+          <div className='basis-1/2 lg:order-2 flex justify-center items-center'>
+            <h1 className='font-bold text-4xl text-[#8A00FF]'>My Skills :</h1>
+          </div>
+          <div  className='basis-1/2 lg:order-1 flex flex-col justify-center items-center'>
+              <p className={`flex justify-center items-center font-bold text-2xl ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
+                Hi, Im a Fullstack Developer and an Ai/Ml enthusiast from Maharashtra , India. 
+              </p>
+            <br />
+              <ul className={` list-disc  justify-center items-center text-xl ${changecolor ? 'text-[#000000]' : 'text-[#ffffff]'}`}>
+                  <div className={`pl-6  grid justify-center items-center `} >
+                    <li className='list-disc p-1'>
+                      C++
+                    </li>
+                    <li className='list-disc p-1'>
+                      Python
+                    </li>
+                    <li className='list-disc p-1'>
+                      JavaScript
+                    </li>
+                    <li className='list-disc p-1'>
+                      Html5
+                    </li>
+                    <li className='list-disc p-1'>
+                      CSS(tailwind)
+                    </li>
+                    <li className='list-disc p-1'>
+                      SQL
+                    </li>
+                    <li className='list-disc p-1'>
+                      MongoDB
+                    </li>
+                  </div>
+              </ul>
+          </div>
+        </div>
+      </div>
+      </>
     );
   }
